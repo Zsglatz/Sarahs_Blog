388e723a929cd790cd6337aef80175e42d858850
Glatz <zsglatz@gmail.com>
2018-01-28 20:15:11 -0700

    Ran resource generator

---
+++ db/migrate/20180129031412_create_blogs.rb
@@ -0,0 +1,12 @@
+class CreateBlogs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.date :date
+      t.text :body
+      t.text :main_image
+
+      t.timestamps
+    end
+  end
+end
